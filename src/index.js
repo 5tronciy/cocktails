98c66573d2553da853b9d30c7b2c2d341c98ae19
Roman Golchuk <rgolchuk@masslight.com>
2023-09-10 16:38:39 +0200

    feat: add normalize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.js';
+import './normalize.css';
 
 document.body.innerHTML = '<div id="root"></div>';
 
